9cf28ce83ba715d1ef022cc45c2dac819a0e7ed6
onajite-taire <onajitetaire@gmail.com>
2019-11-07 23:27:25 -0600

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http'
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
+  
   private _albumUrl = '..assets/album.json';
 
   constructor(private _http: Http) { }
